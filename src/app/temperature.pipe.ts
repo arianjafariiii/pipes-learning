122519e582c539336a7d321740d696254eddc853
ArianJafari <arian.jafari2233@gmail.com>
2025-12-26 09:24:58 +0330

    Use better approach and not to use pipes here

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
     name: 'temp',
-    standalone: true
+    standalone: true,
+    pure: false
 })
 
 export class TemperaturePipe implements PipeTransform {
